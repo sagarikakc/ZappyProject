34a3b269a64ad1855f00f38b871913db6b8dd3eb
sagarikakc <62684429+sagarikakc@users.noreply.github.com>
2021-03-28 18:08:04 -0500

    Add files via upload

---
+++ ProjectZappy/src/main/java/firmfoundation307501/HelloAppEngine.java
@@ -0,0 +1,26 @@
+package firmfoundation307501;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "ZappyProject",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print(" 	Welcome to Zappy Project\r\n");
+
+  }
+}
